acd97d446bbb43b0322657e59f03dd499cfdfe9c
Dawood <dawoodqurashi448@gmail.com>
2023-08-26 12:10:45 +0500

    ok

---
+++ android/app/src/main/kotlin/com/example/bio_metrics/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.bio_metrics
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
